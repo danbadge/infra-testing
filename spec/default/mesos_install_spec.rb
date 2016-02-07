0895e39fc35d3bf10bdfbfb928d7433e9275af06
Daniel Watts <danbadge@gmail.com>
2016-02-07 13:06:07 +0000

    added empty mesos role and failing specs

---
+++ spec/default/mesos_install_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe package('mesos-master') do
+  it { should be_installed }
+end
+
+describe service('mesos-master') do
+  it { should be_enabled }
+  it { should be_running }
+end
